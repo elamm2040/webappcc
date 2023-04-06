382c61a44cfe04eee98516c3fa19c36d40c86bc3
elamm <elamm.2040@gmail.com>
2023-04-06 15:37:00 -0600

    Instalado Redux Arreglado problema en Router (v6 ya no usa commponet)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-import  List from "./componentes/listar_db";
-import { Route, BrowserRouter as Router, Routes, Link } from 'react-router-dom';
+import List from "./componentes/listar_db";
+import { Route, BrowserRouter as Router, Routes, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <div className="container">
         <Routes>
-          <Route exact path="/" component={List}></Route>
+          <Route path="/" element={<List />} />
         </Routes>
       </div>
     </Router>
--- src/componentes/listar_db.js
+++ src/componentes/listar_db.js
@@ -1,42 +1,44 @@
-import React from 'react';
+import React from "react";
 
 class List extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    loadData(){
-        //carga los datos de la bd en formato json, usar API
-    }
+  loadData() {
+    //carga los datos de la bd en formato json, usar API
+  }
 
-    componentDidMount(){
-        this.loadData();
-    }
+  componentDidMount() {
+    this.loadData();
+  }
 
-    render() {
-        return (<table class="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Client</th>
-                    <th>Candidate</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td ></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-            </tbody>
-        </table>);
-    }
+  render() {
+    return (
+      <table class="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Client</th>
+            <th>Candidate</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
 }
 
 export default List;
