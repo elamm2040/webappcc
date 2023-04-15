8bde9b96ae75ab39eaf870a5c9cf9082077327f8
Claudia Cardenas <ccardenaspassiuri@gmail.com>
2023-04-14 20:01:36 -0500

    pantalla de cards sin cabecera ni menu

--- src/componentes/listar_db.js
+++ src/componentes/listar_db.js
@@ -3,6 +3,10 @@ import ListarDbModel from "./models/listar-db.model";
 import { useSelector, useDispatch } from "react-redux";
 import { addAllShifts } from "../app/features/shifts-list";
 import environment from "../environment/environment";
+import {  Button, Card, Col, Row, Steps } from 'antd';
+import { ContainerOutlined } from  '@ant-design/icons';
+import { format } from 'date-fns';
+import './models/listar-db.model.css';
 
 export default function List() {
   const shouldLog = useRef(true);
@@ -24,27 +28,45 @@ export default function List() {
   }, [dispatch, shiftsRedux, env]);
 
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Client</th>
-          <th>Candidate</th>
-          <th>Day</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
+    <section className="timesheetDays">
+
+      <button className="add_button"> + Add Shift </button>
+
+      <Row gutter={16}>
+        { 
           shiftsRedux.map(el => (
-            <tr key={el.id}>
-              <td>{ el.id }</td>
-              <td>{ el.client }</td>
-              <td>{ el.candidate }</td>
-              <td>{ env.days[el.day] }</td>
-            </tr>
+            <Col span={8} className="col">
+                <Card className="card"  bordered={false}>
+                  <h3> { env.days[el.day] } { format(new Date(el.date), 'do MMMM') } </h3>
+                  <p className="duration"> { (((el.finishTime - el.startTime - el.break)).toString()).slice(0,1) } hours </p>
+                  <p> { el.location } </p> 
+                  <p> { el.sublocation } </p>
+                  
+                  <Steps className="steps"
+                    progressDot
+                    size="small"
+                    items={[
+                      {
+                        title: 'Start Time',
+                        status: 'finish',
+                        description:  el.startTime.slice(0,2) + ':' + el.startTime.slice(2,4) 
+                      },
+                      {
+                        title: 'Finish Time',
+                        status: 'finish',
+                        description: el.finishTime.slice(0,2) + ':' + el.finishTime.slice(2,4)
+                      },
+                    ]}
+                  />
+
+                  <p className="expenses"> <ContainerOutlined /> 0 Expenses </p>
+                  
+                  <button className="button" type="primary"> View Shift </button>
+                </Card>
+            </Col>
           ))
         }
-      </tbody>
-    </table>
+      </Row>
+    </section>
   );
 }
