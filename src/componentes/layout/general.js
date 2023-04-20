33e74736b5258702bbedc5ffad452cec79559101
elamm <elamm.2040@gmail.com>
2023-04-19 19:42:19 -0600

    iconos agregados en el layout general
    se agregaron los 4 iconos del footer

--- src/componentes/layout/general.js
+++ src/componentes/layout/general.js
@@ -1,14 +1,45 @@
 import React from "react";
 import './layout.css';
+import { CalendarOutlined, SendOutlined, MessageOutlined, FileTextOutlined } from  '@ant-design/icons';
 
 export default function General(props) {
+    const tableWidth = "100%";
     return (
         <>
             <header className="empAppHeader"></header>
-            {props.children}
+            { props.children }
             <section className="fixedFooter">
                 <button className="send_button"> Send for Approval </button>
-                <div className="mobileSection"></div>
+                <div className="mobileSection">
+                    <table width={ tableWidth }>
+                        <tr>
+                            <td style={{ paddingTop: '20px' }}>
+                                <CalendarOutlined style={{ fontSize: '35px' }} />
+                                <p>
+                                    Shifts
+                                </p>
+                            </td>
+                            <td style={{ paddingTop: '20px' }}>
+                                <SendOutlined style={{ fontSize: '35px' }} />
+                                <p>
+                                    Opportunities
+                                </p>
+                            </td>
+                            <td style={{ paddingTop: '20px' }}>
+                                <MessageOutlined style={{ fontSize: '35px' }} />
+                                <p>
+                                    Messages
+                                </p>
+                            </td>
+                            <td style={{ paddingTop: '20px' }}>
+                                <FileTextOutlined style={{ fontSize: '35px' }} />
+                                <p>
+                                    Compliance
+                                </p>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
             </section>
         </>
     );
